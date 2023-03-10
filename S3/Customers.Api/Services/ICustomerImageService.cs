87b9ab51e2faea0b7e5a6ab891783dfe68fd16d0
Leonardo Vinicius Güths <leonardoguths@gmail.com>
2023-03-10 19:12:03 -0300

    created S3 projects

---
+++ S3/Customers.Api/Services/ICustomerImageService.cs
@@ -0,0 +1,11 @@
+﻿using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http;
+
+namespace Customers.Api.Services;
+
+public interface ICustomerImageService
+{
+    Task<PutObjectResponse> UploadImageAsync(Guid id, IFormFile file);
+    Task<GetObjectResponse> GetImageAsync(Guid id);
+    Task<DeleteObjectResponse> DeleteImageAsync(Guid id);
+}
