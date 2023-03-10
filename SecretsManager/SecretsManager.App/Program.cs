f1d8015e897705fd272bbde9e65658ec1f86d5a2
Leonardo Vinicius Güths <leonardoguths@gmail.com>
2023-03-10 19:53:22 -0300

    create SecretsManager projects

---
+++ SecretsManager/SecretsManager.App/Program.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+var secretsManagerCliente = new AmazonSecretsManagerClient();
+
+var listSecretVersionsRequest = new ListSecretVersionIdsRequest
+{
+    SecretId = "ApiKey",
+    IncludeDeprecated = true
+};
+var listSecretVersionsResponse = await secretsManagerCliente.ListSecretVersionIdsAsync(listSecretVersionsRequest);
+
+var getSecretValueRequest = new GetSecretValueRequest
+{
+    SecretId = "ApiKey"
+};
+var getSecretValueResponse = await secretsManagerCliente.GetSecretValueAsync(getSecretValueRequest);
+
+Console.WriteLine(getSecretValueResponse.SecretString);
+
+var describeSecretRequest = new DescribeSecretRequest
+{
+    SecretId = "ApiKey"
+};
+
+var describeResponse = await secretsManagerCliente.DescribeSecretAsync(describeSecretRequest);
